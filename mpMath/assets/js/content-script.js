933d6749558b8027f7459d9cd5cdef6d5b89bfc9
CPunisher <1343316114@qq.com>
2020-02-17 14:22:31 +0800

    修复SVG功能

--- mpMath/assets/js/content-script.js
+++ mpMath/assets/js/content-script.js
@@ -7,7 +7,7 @@ function formulaClick(event) {
 }
 
 function fixClick(event) {
-    alert('修复公式还在施工!')
+    revise();
     $('.tpl_dropdown_menu', '.formula').css('display', 'none');
     event.stopPropagation();
 }
@@ -50,12 +50,12 @@ chrome.extension.sendMessage({}, function (response) {
                 formulaMenu.setAttribute('class', 'tpl_item tpl_item_dropdown jsInsertIcon formula');
                 formulaMenu.id = 'js_editor_insert_formula';
                 $(formulaMenu).append('<span>公式</span>');
-                
+
                 // 分别为 下拉菜单栏、插入公式、修复SVG、指南
                 let dropdownMenu = document.createElement('ul');
                 dropdownMenu.setAttribute('class', 'tpl_dropdown_menu');
                 dropdownMenu.style.display = 'none';
-                
+
                 let formulaInsertItem = document.createElement('li');
                 formulaInsertItem.setAttribute('class', 'tpl_dropdown_menu_item');
                 formulaInsertItem.innerText = '插入公式 ⌘/';
@@ -73,7 +73,7 @@ chrome.extension.sendMessage({}, function (response) {
                 formulaGuide.innerText = '指南';
                 formulaGuide.onclick = guideClick;
                 dropdownMenu.appendChild(formulaGuide);
-                
+
                 formulaMenu.appendChild(dropdownMenu);
                 $(formulaMenu).click(function () {
                     $(dropdownMenu).css('display', '');
@@ -93,3 +93,46 @@ chrome.extension.sendMessage({}, function (response) {
         }
     }, 10);
 });
+
+/*
+以下代码源于 https://github.com/kongxiangyan/bookmarklet
+修复修正微信公众号图文编辑器粘贴 SVG 时部分转换为 Embed 导致不支持 Dark Mode 的问题
+*/
+function loadSVG(src) {
+    return new Promise((resolve) => {
+        let ajax = new XMLHttpRequest();
+        ajax.open('GET', src, true);
+        ajax.send();
+        ajax.onload = function(e) {
+            let div = document.createElement('div');
+            div.innerHTML = ajax.responseText;
+            let svg = div.childNodes[1];
+            resolve(svg);
+        }
+    })
+}
+
+function revise() {
+    console.log(`【MP_SVG_REVISE】 Start`);
+    let ueditor = document.getElementById('ueditor_0');
+    let view = ueditor.contentDocument.getElementsByClassName('view')[0];
+    let embeds = view.querySelectorAll('embed');
+    console.log(`【MP_SVG_REVISE】 检测到 ${embeds.length} 个目标……`);
+    let promises = [];
+    embeds.forEach((embed, index) => {
+        console.log(`【MP_SVG_REVISE】 第 ${index} 个……`);
+        let parent_node = embed.parentNode;
+        promises.push(new Promise(resolve => {
+            loadSVG(embed.src).then(svg => {
+                parent_node.insertBefore(svg, embed);
+                parent_node.removeChild(embed);
+                resolve();
+            })
+        }))
+    });
+    Promise.all(promises).then(() => {
+        console.log('Revise complete！');
+        //alert('Revise complete！');
+        alert(`修复了 ${embeds.length} 个目标!`);
+    })
+}
